afb56acaecbd6b68603be2d4483b85aebbd86487
Lara Boarro <51834635+LaraBoarro@users.noreply.github.com>
2021-07-07 23:14:12 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -110,3 +110,45 @@ $(document).ready(function() {
 	$('.troca-imagem').hover( showImg, offImg);
 
 });
+
+var geocoder;
+var map;
+var marker;
+
+  function initialize() {
+    var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+    var options = {
+        zoom: 5,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("mapa"), options);
+
+    geocoder = new google.maps.Geocoder();
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+
+    marker.setPosition(latlng);
+}
+
+// verifica se o navegador tem suporte a geolocalização
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){ // callback de sucesso
+        // ajusta a posição do marker para a localização do usuário
+        marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    }, 
+    function(error){ // callback de erro
+       alert('Erro ao obter localização!');
+       console.log('Erro ao obter localização.', error);
+    });
+} else {
+    console.log('Navegador não suporta Geolocalização!');
+}
+
+$(document).ready(function () {
+    initialize();
+});
